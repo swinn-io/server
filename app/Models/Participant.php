00a6d1d4ee5901d3309059c45fd0bfb194d430ba
Burak <burakalbayrak@live.com>
2020-07-31 12:17:40 +0300

    redefining date fields

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -8,4 +8,11 @@ use Cmgmyr\Messenger\Models\Participant as BaseParticipant;
 class Participant extends BaseParticipant
 {
     use HasUUID;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['last_read', 'created_at', 'updated_at', 'deleted_at'];
 }
