cf6a1ef431876cd656a73dac184308234085067d
Burak <burakalbayrak@live.com>
2020-07-31 12:16:17 +0300

    add some recipients without messages and mark as read for some of the participants

--- database/seeds/MessagingSeeder.php
+++ database/seeds/MessagingSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Models\Message;
 use App\Models\Thread;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class MessagingSeeder extends Seeder
@@ -14,10 +15,28 @@ class MessagingSeeder extends Seeder
     public function run()
     {
         $threads = factory(Thread::class, 150)->create();
-        $threads->each(function ($item) {
-            $messages = factory(Message::class, rand(1, 25))->create(['thread_id' => $item->id]);
-            $users = $messages->pluck('user_id')->unique()->toArray();
-            $item->addParticipant($users);
+        $threads->each(function ($thread) {
+            /**
+             * @var Thread $thread
+             */
+            $messages = factory(Message::class, rand(1, 25))->create(['thread_id' => $thread->id]);
+
+            // Set message sender as participants.
+            $users_ids = $messages->pluck('user_id');
+
+            // Add random recipients
+            User::inRandomOrder()->limit(rand(0, 5))->get()->pluck('id')->each(function ($recipient) use ($users_ids) {
+                $users_ids->push($recipient);
+            });
+
+            // Add participants
+            $thread->addParticipant($users_ids->unique()->toArray());
+
+            // Mark as read for some the participant users and choose some lucky ones.
+            $some_users = rand(0, $users_ids->count());
+            $users_ids->random($some_users)->each(function ($user) use ($thread) {
+                $thread->markAsRead($user);
+            });
         });
     }
 }
