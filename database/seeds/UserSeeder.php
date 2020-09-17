0639cd9dd6c82835fb9bf418d54c7d1ba5bd24b0
ikidnapmyself <burakalbayrak@live.com>
2020-09-17 22:22:31 +0300

    create client for each fake user

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,9 +1,26 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Laravel\Passport\ClientRepository;
 
 class UserSeeder extends Seeder
 {
+    /**
+     * Laravel Passport Client Repository.
+     *
+     * @var ClientRepository
+     */
+    private ClientRepository $clientRepository;
+
+    /**
+     * LoginService constructor.
+     *
+     * @param ClientRepository $repository
+     */
+    public function __construct(ClientRepository $repository)
+    {
+        $this->clientRepository = $repository;
+    }
     /**
      * Run the database seeds.
      *
@@ -11,6 +28,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Models\User::class, 50)->create();
+        factory(\App\Models\User::class, 50)->create()->each(function ($user){
+            $this->clientRepository->create(
+                $user->id,
+                "{$user->provider_name}-{$user->provider_id}",
+                'uri://',
+                $user->provider_name,
+                true
+            );
+        });
     }
 }
