06c9c9bc9c394c708d23e9178690f7ddefe6781f
ikidnapmyself <burakalbayrak@live.com>
2020-11-03 23:50:52 +0300

    online users

--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (string) $user->id === (string) $id;
 });
+
+Broadcast::channel('online', function ($user) {
+    if (auth()->check()) {
+        return $user->toArray();
+    }
+});
