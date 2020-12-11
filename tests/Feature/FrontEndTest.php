2e796c19555b68963c93a4ca5c2cccc0f8bccf32
ikidnapmyself <burakalbayrak@live.com>
2020-12-11 08:11:32 +0300

    front end controller test

---
+++ tests/Feature/FrontEndTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FrontEndTest extends TestCase
+{
+    /**
+     * Test if home page is there.
+     *
+     * @return void
+     */
+    public function testFrontEndControllerWelcomeMethod()
+    {
+        $response = $this->get('/');
+
+        $response->assertOk();
+        $response->assertSee(config('app.name'));
+    }
+}
