882c62aa6a5e3677fb3eed354dcabde2e6417f0d
ikidnapmyself <burakalbayrak@live.com>
2020-12-11 08:02:16 +0300

    login tests

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
+use Laravel\Socialite\Contracts\User as SocialiteUser;
+use Mockery;
+use Laravel\Socialite\Facades\Socialite;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use WithFaker;
+
+    /**
+     * Setup testing.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('passport:install');
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testLoginControllerHomeMethod()
+    {
+        $response = $this->get(route('login'));
+
+        $response->assertOk();
+        $response->assertSee('GitHub');
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testLoginControllerRedirectMethod()
+    {
+        $response = $this->get(route('login.redirect', ['provider' => 'github']));
+
+        $response->assertRedirect();
+        $response->assertSee('Redirecting to');
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testLoginControllerCallbackMethod()
+    {
+        $user = User::factory()->make();
+        $socialiteUser = Mockery::mock(SocialiteUser::class);
+        $socialiteUser->shouldReceive([
+            'getId'       => Str::random(),
+            'getName'     => $user->name,
+            'getEmail'    => $this->faker->email,
+            'getNickname' => Str::slug($user->name),
+            'getAvatar'   => $this->faker->url,
+        ])
+            ->andSet('user', $user)
+            ->andSet('token', Str::random(40))
+            ->andSet('refreshToken', Str::random(40));
+
+        Socialite::shouldReceive('driver->user')->andReturn($socialiteUser);
+
+        $response = $this->get(route('login.callback', ['provider' => 'github']));
+        $response->assertRedirect();
+    }
+}
