5e43437198f4b3326d66312154a8c33ba5dfd5bc
ikidnapmyself <burakalbayrak@live.com>
2020-12-17 22:11:17 +0300

    pagination

--- resources/views/livewire/message/index.blade.php
+++ resources/views/livewire/message/index.blade.php
@@ -1,31 +1,30 @@
 <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ __('Contacts') }}
+        {{ __('Messages') }}
     </h2>
 </x-slot>
-
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="flex flex-col">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        @if($contacts->total() > $contacts->perPage())
-                            <div class="bg-white px-4 py-3 border-t border-gray-200 sm:px-6">
-                                {{ $contacts->links() }}
+                        @if($messages->total() > $messages->perPage())
+                            <div class="bg-white px-4 py-3  border-t border-gray-200 sm:px-6">
+                                {{ $messages->links() }}
                             </div>
                         @endif
                         <table class="min-w-full divide-y divide-gray-200">
                             <thead class="bg-gray-50">
                             <tr>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    {{ __('Name') }}
+                                    {{ __('Subject') }}
                                 </th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    {{ __('Source') }}
+                                    {{ __('Participants') }}
                                 </th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    {{ __('Updated At') }}
+                                    {{ __('Last Message') }}
                                 </th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     {{ __('Created At') }}
@@ -36,44 +35,41 @@
                             </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach ($contacts as $key => $contact)
+                            @foreach ($messages as $key => $message)
                                 <tr>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <div class="flex items-center">
-                                            <div class="flex-shrink-0 h-10 w-10">
-                                                <img class="h-10 w-10 rounded-full" src="{{ $contact->source->profile_photo_url }}" alt="{{ $contact->source->name }}">
-                                            </div>
                                             <div class="ml-4">
                                                 <div class="text-sm font-medium text-gray-900">
-                                                    {{ $contact->source->name }}
+                                                    {{ $message->subject }}
                                                 </div>
                                                 <div class="text-sm text-gray-500">
-                                                    {{ $contact->source->email }}
+                                                    {{ 'Created by Someone' }}
                                                 </div>
                                             </div>
                                         </div>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="text-sm text-gray-900">{{ $contact->source->provider_name }}</div>
+                                        <div class="text-sm text-gray-900">{{ $message->participants->count() }}</div>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        {{ $contact->source->updated_at }}
+                                        {{ 'Last Update' }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                        {{ $contact->source->created_at }}
+                                        {{ $message->created_at }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">{{ __('Mutual Threads') }}</a>
+                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">{{ __('Show History') }}</a>
                                         <br>
-                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">{{ __('Edit Contact') }}</a>
+                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">{{ __('New Message') }}</a>
                                     </td>
                                 </tr>
                             @endforeach
                             </tbody>
                         </table>
-                        @if($contacts->total() > $contacts->perPage())
-                            <div class="bg-white px-4 py-3 border-t border-gray-200 sm:px-6">
-                                {{ $contacts->links() }}
+                        @if($messages->total() > $messages->perPage())
+                            <div class="bg-white px-4 py-3  border-t border-gray-200 sm:px-6">
+                                {{ $messages->links() }}
                             </div>
                         @endif
                     </div>
