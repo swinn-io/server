f21b2a38b817b168c16eb62ed6f3d4f645f506f8
ikidnapmyself <burakalbayrak@live.com>
2021-04-20 00:42:55 +0300

    show qr

--- resources/views/livewire/contact/index.blade.php
+++ resources/views/livewire/contact/index.blade.php
@@ -22,6 +22,9 @@
                                     {{ __('Name') }}
                                 </th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    {{ __('UUID') }}
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     {{ __('Source') }}
                                 </th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -41,17 +44,27 @@
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <div class="flex items-center">
                                             <div class="flex-shrink-0 h-10 w-10">
-                                                <img class="h-10 w-10 rounded-full" src="{{ $contact->source->profile_photo_url }}" alt="{{ $contact->source->name }}">
+                                                <a href="{{ route('contact.show', ['id' => $contact->id]) }}">
+                                                    <img class="h-10 w-10 rounded-full" src="{{ $contact->source->profile_photo_url }}" alt="{{ $contact->source->name }}">
+                                                </a>
                                             </div>
                                             <div class="ml-4">
                                                 <div class="text-sm font-medium text-gray-900">
-                                                    {{ $contact->source->name }}
+                                                    <a href="{{ route('contact.show', ['id' => $contact->id]) }}">
+                                                        {{ $contact->source->name }}
+                                                    </a>
                                                 </div>
                                                 <div class="text-sm text-gray-500">
                                                     {{ $contact->source->email }}
                                                 </div>
                                             </div>
                                         </div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-center" x-data="{ open: false }">
+                                        <x-jet-button x-show="!open" @click="open = true">{{ __('Show QR') }}</x-jet-button>
+                                        <span x-show="open" @click="open = false">
+                                            @component('components.qrcode', ['content' => $contact->id, 'size' => 150])@endcomponent
+                                        </span>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <div class="text-sm text-gray-900">{{ $contact->source->provider_name }}</div>
