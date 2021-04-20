14e727d6e1150e8a3970272e155556bf6c20bc3b
ikidnapmyself <burakalbayrak@live.com>
2021-04-20 20:51:12 +0300

    route fix

--- resources/views/livewire/contact/index.blade.php
+++ resources/views/livewire/contact/index.blade.php
@@ -44,13 +44,13 @@
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <div class="flex items-center">
                                             <div class="flex-shrink-0 h-10 w-10">
-                                                <a href="{{ route('contact.show', ['id' => $contact->id]) }}">
+                                                <a href="{{ route('contact.display', ['id' => $contact->id]) }}">
                                                     <img class="h-10 w-10 rounded-full" src="{{ $contact->source->profile_photo_url }}" alt="{{ $contact->source->name }}">
                                                 </a>
                                             </div>
                                             <div class="ml-4">
                                                 <div class="text-sm font-medium text-gray-900">
-                                                    <a href="{{ route('contact.show', ['id' => $contact->id]) }}">
+                                                    <a href="{{ route('contact.display', ['id' => $contact->id]) }}">
                                                         {{ $contact->source->name }}
                                                     </a>
                                                 </div>
