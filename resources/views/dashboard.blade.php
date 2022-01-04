4a0e50128ec6ec503be3dd3dd877bc13c1fb8a92
ikidnapmyself <burakalbayrak@live.com>
2022-01-05 02:00:34 +0300

    threads

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,11 +10,20 @@
                             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                                 <span class="font-semibold title-font text-gray-700">CATEGORY</span>
                                 <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+                                <div class="flex -space-x-3 overflow-hidden">
+                                    @foreach($thread->participants as $participant)
+                                        <img class="relative z-30 hover:z-40 inline object-cover w-12 h-12 border-2 border-white rounded-full" src="https://ui-avatars.com/api/?name={{ $participant->user->name }}&color=7F9CF5&background=EBF4FF" alt="{{ $participant->user->name }}&color=7F9CF5&background=EBF4FF">
+                                    @endforeach
+                                </div>
                             </div>
                             <div class="md:flex-grow">
-                                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{{ $thread->subject }}</h2>
+                                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
+                                    {{ $thread->subject }}
+                                    <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-sm font-bold leading-none text-red-100 bg-red-600 rounded-full">9</span>
+                                </h2>
                                 <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-                                <a class="text-indigo-500 inline-flex items-center mt-4" href="{{ route('thread.show', ['thread' => $thread->id]) }}">Learn More
+                                <a class="text-indigo-500 inline-flex items-center mt-4" href="{{ route('thread.show', ['thread' => $thread->id]) }}">
+                                    History
                                     <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path d="M5 12h14"></path>
                                         <path d="M12 5l7 7-7 7"></path>
--- resources/views/thread.blade.php
+++ resources/views/thread.blade.php
@@ -25,25 +25,15 @@
                     <div class="-my-8 divide-y-2 divide-gray-100">
                         <div class="py-8 flex flex-wrap md:flex-nowrap">
                             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                                <span class="font-semibold title-font text-gray-700">CATEGORY</span>
-                                <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-                                <div class="-space-x-4">
-                                    <img class="relative z-30 inline object-cover w-12 h-12 border-2 border-white rounded-full" src="https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
-                                    <img class="relative z-20 inline object-cover w-12 h-12 border-2 border-white rounded-full" src="https://images.pexels.com/photos/2955305/pexels-photo-2955305.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
-                                    <img class="relative z-10 inline object-cover w-12 h-12 border-2 border-white rounded-full" src="https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
+                                <span class="font-semibold title-font text-gray-700">{{ $message->user->name }}</span>
+                                <span class="mt-1 text-gray-500 text-sm">{{ $message->user->created_at }}</span>
+                                <div class="">
+                                    <img class="relative z-10 inline object-cover w-20 h-20 border-2 border-white rounded-full" src="https://ui-avatars.com/api/?name={{ $message->user->name }}&color=7F9CF5&background=EBF4FF" alt="Profile image"/>
                                 </div>
                             </div>
                             <div class="md:flex-grow">
-                                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
-                                    {{ $message->user->name }}
-                                </h2>
+                                <p class="mt-0 text-xs mb-2">{{ $message->user->created_at }}</p>
                                 <p class="leading-relaxed">{{ print_r($message->body, true) }}</p>
-                                <a class="text-indigo-500 inline-flex items-center mt-4" href="">Learn More
-                                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M5 12h14"></path>
-                                        <path d="M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
                             </div>
                         </div>
                     </div>
